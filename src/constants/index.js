d511232579cbbc531aed4629d02a84c6e23de70b
Hassan Sanni <zansanni@gmail.com>
2025-01-05 15:26:25 -0800

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,17 +8,32 @@ export const HERO_CONTENT = `I am a passionate full stack developer with a knack
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
-  {
-    year: "2022 - Present",
-    role: "Manager",
-    company: "VanSuya",
-    description: `Developed and implemented a customer feedback program, leading to an 8% increase in positive customer
-reviews and a 10% improvement in overall customer satisfaction. Implemented a new inventory management system using Microsoft Excel to track perishable goods, reducing
-waste by 15%.`,
-    technologies: ["Microsoft Office", "Interpersonal Skill", "Communications"],
+    {
+    year: "Jan 2024 - Present",
+    role: "Web Developer",
+    company: "Freelance",
+    description: `Collaborated with clients to gather requirements and translate them into functional web applications. Developed responsive and visually appealing websites using HTML, CSS, JavaScript, and Bootstrap.
+- Conducted rigorous testing to ensure cross-browser compatibility and optimal performance.`,
+    technologies: ["Java", "React", "HTML/CSS", "IT Services", "Technical Support"],
+  },s
+    {
+    year: "Sept 2023 - Jan 2024",
+    role: "Software Engineer",
+    company: "VanSuya Food Corps",
+    description: `Inventory Management Systems. Develop systems to manage inventory effectively in automating inventory tracking, reduce waste, and integrate with ordering and POS systems.
+- Created a Website and Digital Presence. Develop and maintain the restaurant's website, the website is user-friendly, includes online ordering capabilities, and integrates with social media and marketing tools.`,
+    technologies: ["AWS System", "Algorithms", "React Native", "Data Entry"],
+  },
+    {
+    year: "May 2023 - Aug 2023",
+    role: "Intern",
+    company: "Hybis Brand",
+    description: `Resolved 15+ bugs in microservices by communicating with stakeholders, finding root causes, and developing fixes. Developing using Javascript
+- Analyzing customer feedback data to identify trends and areas for improvement. Assisting in the development of a mobile app for customer loyalty programs..`,
+    technologies: ["JavaScript", "HTML/CSS", "React", "Data Entry"],
   },
   {
-    year: "2020 - 2021",
+    year: "Apr 2021 - Sept 2021",
     role: "Teaching Assistance",
     company: "CompuDemic",
     description: `Leading office teaching hours for over 30 students in the introduction to Python and Computer System
